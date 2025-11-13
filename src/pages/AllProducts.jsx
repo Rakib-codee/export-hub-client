837c9fa143520209fc77669288e5082a26bc5834
Md Mahfujur Rahman Rakib <rakibislam4913@gmail.com>
2025-11-13 10:46:01 +0800

    fix warrings AllProduct page

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -87,7 +87,7 @@ const AllProducts = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 px-4 md:px-8 py-12">
+    <div className="min-h-screen bg-linear-to-br from-gray-50 to-blue-50 px-4 md:px-8 py-12">
       <h1 className="text-4xl font-extrabold mb-8 text-center text-blue-700">
         All Products
       </h1>
